22513c27a5407d9c74e768cb081c8b17fda04dce
Angel Rego <angelrego@gmail.com>
2022-07-28 12:14:45 +0200

    kyber: main example code using the kyber static library

---
+++ src/electrocomms.h
@@ -0,0 +1,34 @@
+/**
+ * Header for interoperability
+ *
+ * When chosing another algorithm, change the names only in this header,
+ * so the C code calls an variables will not be refactored.
+ *
+ */
+#ifndef ELECTROCOMMS_H
+#define ELECTROCOMMS_H
+#include <stdint.h>
+
+#include "kyber1024/api.h"
+
+#define KEM_SKL PQCLEAN_KYBER1024_CLEAN_CRYPTO_SECRETKEYBYTES
+#define KEM_PKL PQCLEAN_KYBER1024_CLEAN_CRYPTO_PUBLICKEYBYTES
+#define KEM_CTL PQCLEAN_KYBER1024_CLEAN_CRYPTO_CIPHERTEXTBYTES
+#define KEM_SSL PQCLEAN_KYBER1024_CLEAN_CRYPTO_BYTES
+
+#define KEM_NAME PQCLEAN_KYBER1024_CLEAN_CRYPTO_ALGNAME
+
+inline int KEM_GENKEYS(uint8_t *pk, uint8_t *sk) {
+  return PQCLEAN_KYBER1024_CLEAN_crypto_kem_keypair(pk, sk);
+}
+
+inline int KEM_ENC(const uint8_t *pk, uint8_t *ss, uint8_t *ct) {
+  return PQCLEAN_KYBER1024_CLEAN_crypto_kem_enc(ct, ss, pk);
+}
+
+inline int KEM_DEC(const uint8_t *sk, const uint8_t *ct, uint8_t *ss) {
+  return PQCLEAN_KYBER1024_CLEAN_crypto_kem_dec(ss, ct, sk);
+}
+
+void log8(char *text, uint8_t *data, size_t len);
+#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "electrocomms.h"
+#include "kyber1024/api.h"
 
 // int main(int argc, char const *argv[]) {
 int main() {
-  printf("test hello\n");
-  return 0;
+  uint8_t pk[KEM_PKL];
+  uint8_t sk[KEM_SKL];
+  int status = KEM_GENKEYS(pk, sk);
+  log8("pk: ", pk, KEM_PKL);
+  printf("status=%d\n", status);
+  return status;
+}
+
+void log8(char *text, uint8_t *data, size_t len) {
+  // size_t LIMIT = len;
+  size_t LIMIT = len < 32 ? len : 32;
+  printf("%s", text);
+  for (size_t r = 0; r < LIMIT; r++) printf("%02x", *data++);
+  if (len > LIMIT) printf("...%zu bytes", len);
+  printf("\n");
 }
