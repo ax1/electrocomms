17ec78b2749f9f5a6f812fadc360279790b94c25
Angel Rego <angelrego@gmail.com>
2022-08-02 02:42:31 +0200

    util: fix using strlen or sizeof for input

--- src/util.c
+++ src/util.c
@@ -4,6 +4,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+
+     ██╗ ██╗    ██╗  ██╗███████╗██╗  ██╗       ██╗
+    ██╔╝██╔╝    ██║  ██║██╔════╝╚██╗██╔╝       ██║
+   ██╔╝██╔╝     ███████║█████╗   ╚███╔╝     ████████╗
+  ██╔╝██╔╝      ██╔══██║██╔══╝   ██╔██╗     ██╔═██╔═╝
+ ██╔╝██╔╝       ██║  ██║███████╗██╔╝ ██╗    ██████║
+ ╚═╝ ╚═╝        ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ╚═════╝
+
+     ██╗ ██╗    ██████╗  █████╗ ███████╗███████╗ ██████╗ ██╗  ██╗
+    ██╔╝██╔╝    ██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝ ██║  ██║
+   ██╔╝██╔╝     ██████╔╝███████║███████╗█████╗  ███████╗ ███████║
+  ██╔╝██╔╝      ██╔══██╗██╔══██║╚════██║██╔══╝  ██╔═══██╗╚════██║
+ ██╔╝██╔╝       ██████╔╝██║  ██║███████║███████╗╚██████╔╝     ██║
+ ╚═╝ ╚═╝        ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝      ╚═╝
+
+
+*/
+
 static char encoding_table[] = {
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
@@ -11,15 +30,15 @@ static char encoding_table[] = {
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 static char *decoding_table = NULL;
-static int mod_table[] = {0, 2, 1};
+static size_t mod_table[] = {0, 2, 1};
 
 static void build_decoding_table() {
   decoding_table = malloc(256);
-  for (int i = 0; i < 64; i++)
+  for (size_t i = 0; i < 64; i++)
     decoding_table[(unsigned char)encoding_table[i]] = i;
 }
 
-static void base64_cleanup() { free(decoding_table); }
+/*static void base64_cleanup() { free(decoding_table); }*/
 
 char *base64_encode(const uint8_t *data, size_t input_length,
                     size_t *output_length) {
@@ -28,7 +47,7 @@ char *base64_encode(const uint8_t *data, size_t input_length,
   char *encoded_data = malloc(*output_length);
   if (encoded_data == NULL) return NULL;
 
-  for (int i = 0, j = 0; i < input_length;) {
+  for (size_t i = 0, j = 0; i < input_length;) {
     uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
     uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
     uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
@@ -40,7 +59,7 @@ char *base64_encode(const uint8_t *data, size_t input_length,
     encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
   }
 
-  for (int i = 0; i < mod_table[input_length % 3]; i++)
+  for (size_t i = 0; i < mod_table[input_length % 3]; i++)
     encoded_data[*output_length - 1 - i] = '=';
 
   return encoded_data;
@@ -49,7 +68,7 @@ char *base64_encode(const uint8_t *data, size_t input_length,
 uint8_t *base64_decode(const char *data, size_t input_length,
                        size_t *output_length) {
   if (decoding_table == NULL) build_decoding_table();
-
+  if (data[input_length - 1] == '\0') input_length = input_length - 1;
   if (input_length % 4 != 0) return NULL;
 
   *output_length = input_length / 4 * 3;
@@ -59,7 +78,7 @@ uint8_t *base64_decode(const char *data, size_t input_length,
   unsigned char *decoded_data = malloc(*output_length);
   if (decoded_data == NULL) return NULL;
 
-  for (int i = 0, j = 0; i < input_length;) {
+  for (int i = 0, j = 0; i < (int)input_length;) {
     uint32_t sextet_a =
         data[i] == '=' ? 0 & i++ : decoding_table[(size_t)data[i++]];
     uint32_t sextet_b =
@@ -70,9 +89,9 @@ uint8_t *base64_decode(const char *data, size_t input_length,
         data[i] == '=' ? 0 & i++ : decoding_table[(size_t)data[i++]];
     uint32_t triple = (sextet_a << 3 * 6) + (sextet_b << 2 * 6) +
                       (sextet_c << 1 * 6) + (sextet_d << 0 * 6);
-    if (j < *output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
-    if (j < *output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
-    if (j < *output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+    if (j < (int)*output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+    if (j < (int)*output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+    if (j < (int)*output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
   }
   return decoded_data;
 }
@@ -85,6 +104,28 @@ size_t hex_to_uint8(const char *src, uint8_t *dest) {
     dest[i] = value;
   }
   return i;
+}
+
+/*
+     ██╗ ██╗    ██╗      ██████╗  ██████╗
+    ██╔╝██╔╝    ██║     ██╔═══██╗██╔════╝
+   ██╔╝██╔╝     ██║     ██║   ██║██║  ███╗
+  ██╔╝██╔╝      ██║     ██║   ██║██║   ██║
+ ██╔╝██╔╝       ███████╗╚██████╔╝╚██████╔╝
+ ╚═╝ ╚═╝        ╚══════╝ ╚═════╝  ╚═════╝
+
+*/
+
+/**
+ * Print array or pointer, shorten to 1 line
+ */
+void log8(char *text, uint8_t *data, size_t len) {
+  // size_t LIMIT = len;
+  size_t LIMIT = len < 32 ? len : 32;
+  printf("%s", text);
+  for (size_t r = 0; r < LIMIT; r++) printf("%02x", *data++);
+  if (len > LIMIT) printf("...%zu bytes", len);
+  printf("\n");
 }
 
 // int main(int argc, char const *argv[]) {
