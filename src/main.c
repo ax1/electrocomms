c32c124c880480d745a643c9b2f624c08355aa7c
Angel Rego <angelrego@gmail.com>
2022-08-03 10:28:15 +0200

    main: use server and client format for startup
    Commit as backup because after implementing...maybe the nc-like start format is better an easier to remember

--- src/main.c
+++ src/main.c
@@ -7,20 +7,25 @@
 
 const char help[] =
     "Exchange quantum-resistant safe data.\n\n\
-Parameters: [server|client]\n\
-- server: start listening socket\n\
-- client: start and connect to server\n";
+Parameters: [server | client]\n\
+- server: start listening socket. Format: server [PORT (default 8080)]\n\
+- client: start and connect to server. Format client [[HOST (default 127.0.0.1) [PORT (default 8080)]]\n";
 
 int main(int argc, char const *argv[]) {
   if (argc == 1) {
     printf(help);
   } else {
     const char *mode = argv[1];
-    if (strcmp(mode, "client") == 0)
-      socket_client();
-    else if (strcmp(mode, "server") == 0)
-      socket_server();
-    else {
+    char *HOST = "127.0.0.1";
+    int PORT = 8080;
+    if (strcmp(mode, "client") == 0) {
+      if (argc > 2) HOST = (char *)argv[2];
+      if (argc > 3) PORT = atoi(argv[3]);
+      socket_client(HOST, PORT);
+    } else if (strcmp(mode, "server") == 0) {
+      if (argc > 2) PORT = atoi(argv[2]);
+      socket_server(PORT);
+    } else {
       // printf("Invalid parameters\n\n");
       printf(help);
     }
