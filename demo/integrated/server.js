8f7bd9e79b6e4908cdea676ec8b80a08944d2a4c
Angel Rego <angelrego@gmail.com>
2022-08-23 10:35:50 +0200

    server: using kem server as primary
    This code is not functional because:
    - main socket server should be the traditional comms channel (eg: acting as an http server)
    - one of the sockets must be forked because if same process, the second socket will be rejected

--- demo/integrated/server.js
+++ demo/integrated/server.js
@@ -4,14 +4,31 @@
  * The server starts an electrocomms app as 'server' mode
  * 
  * Whenever a client requests a secure connection key, 
- * by usinng Post-quantum KEM, the key is retrieved in the stdout.
+ * by using Post-Quantum KEM, the key is retrieved in the stdout.
  * Note that the electrocomms in server mode is always alive, to 
  * provide KEM to clients.
  */
 import { spawn } from 'child_process'
+import { createServer } from 'net'
 
-const p = spawn('../../bin/electrocomms', ['8080'], { cwd: '../../bin/' })
 
-p.stdout.on('data', data => console.log(`stdout: ${data}`))
+//---START THE KEM BRIDGE @8080-----
+const p = spawn('./electrocomms', ['8080'], { cwd: '../../bin/' })
+p.stdout.on('data', data => { const key = data.toString().substring(0, data.length - 1); processSecureData(key).catch(console.error) })
 p.stderr.on('data', data => console.error(`stderr: ${data}`))
 p.on('close', code => console.log(`child process exited with code ${code}`))
+
+//--START THE SERVER LISTENER @8081----
+const server = createServer(function (c) {
+  console.log('client connected')
+  c.on('data', data => console.log(data.toString()))
+  c.on('end', () => console.log('client disconnected'))
+  c.on('error', console.error)
+})
+const port = 8081
+server.listen(port)
+console.log('Socket server listening at port ' + port)
+
+async function processSecureData(key) {
+  console.log(`stdout: ${key}`)
+}
